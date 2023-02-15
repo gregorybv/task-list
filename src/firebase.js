8c361136cd2a0b2884a6908a105a48a546797761
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-02-15 10:46:35 +0300

    add firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCd9DDwGFGMsgvHRe82bQq4wjjmzbf3xVU",
+  authDomain: "todo-app-yt-29c8e.firebaseapp.com",
+  projectId: "todo-app-yt-29c8e",
+  storageBucket: "todo-app-yt-29c8e.appspot.com",
+  messagingSenderId: "128198530493",
+  appId: "1:128198530493:web:61b751df00331988a4aed4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
