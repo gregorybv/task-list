d1efff1365eaf8b0f3fb8b3f12189b199b7989d8
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-02-15 10:41:22 +0300

    add style to project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-import './App.css';
+import React, {useState} from "react";
+import {AiOutlinePlus} from "react-icons/ai";
+import Todo from "./Todo";
 
 function App() {
+  const [todos, setTodos] = useState(['Coding React', 'Grind Leetcode'])
+
   return (
-    <div>
+    <div className='h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]'>
+      <div className='bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4'>
+        <h3 className='text-3xl font-bold text-center text-gray-800 p-2'>Todo App</h3>
+        <form className='flex justify-between'>
+          <input className='border p-2 w-full text-xl' type="text" placeholder='Add Todo'/>
+          <button className='border p-4 ml-2 bg-purple-500 text-slate-100'>
+            <AiOutlinePlus size={30}/>
+          </button>
+        </form>
+        <ul>
+          {todos.map((todo, index) => (
+            <Todo key={index} todo={todo}/>
+          ))}
+        </ul>
+        <p className='text-center p-2'>You have 2 todos</p>
+      </div>
     </div>
   );
 }
