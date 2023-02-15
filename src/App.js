5b354867aa2cc18fb8b1fb6d24d6a95222724ff9
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-02-15 11:59:06 +0300

    final to project

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from "react";
 import {AiOutlinePlus} from "react-icons/ai";
 import Todo from "./Todo";
 import {db} from "./firebase";
-import {query, collection, onSnapshot, updateDoc, doc} from 'firebase/firestore'
+import {query, collection, onSnapshot, updateDoc, doc, addDoc, deleteDoc} from 'firebase/firestore'
 
 function App() {
   const [todos, setTodos] = useState(['Coding React', 'Grind Leetcode'])
@@ -14,7 +14,13 @@ function App() {
     e.preventDefault(e)
     if (input === '') {
       alert('Please enter a valid todo')
+      return
     }
+    await addDoc(collection(db, 'todos'), {
+      text: input,
+      completed: false,
+    })
+    setInput('')
   }
 
   // Read todo from firebase
@@ -33,11 +39,15 @@ function App() {
   // Update todo in firebase
   const toggleComplete = async (todo) => {
     await updateDoc(doc(db, 'todos', todo.id), {
-      completed: !todo.completed
+      completed: !todo.completed,
     })
   }
 
   // Delete todo
+  const deleteTodo = async (id) => {
+    await deleteDoc(doc(db, 'todos', id))
+  }
+
 
   return (
     <div className='h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]'>
@@ -56,10 +66,15 @@ function App() {
         </form>
         <ul>
           {todos.map((todo, index) => (
-            <Todo key={index} todo={todo} toggleComplete={toggleComplete}/>
+            <Todo
+              key={index}
+              todo={todo}
+              toggleComplete={toggleComplete}
+              deleteTodo={deleteTodo}/>
           ))}
         </ul>
-        <p className='text-center p-2'>You have 2 todos</p>
+        {todos.length < 1 ? null :
+          <p className='text-center p-2'>{`You have ${todos.length} todos`}</p>}
       </div>
     </div>
   );
