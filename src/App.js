f6d0998d866b95a7ee2543bd4a35e7760cf0ff8d
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-02-15 11:45:13 +0300

    add function in project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import {AiOutlinePlus} from "react-icons/ai";
 import Todo from "./Todo";
+import {db} from "./firebase";
+import {query, collection, onSnapshot, updateDoc, doc} from 'firebase/firestore'
 
 function App() {
   const [todos, setTodos] = useState(['Coding React', 'Grind Leetcode'])
+  const [input, setInput] = useState('')
+  // console.log(input)
+
+  // Create todo
+  const createTodo = async (e) => {
+    e.preventDefault(e)
+    if (input === '') {
+      alert('Please enter a valid todo')
+    }
+  }
+
+  // Read todo from firebase
+  useEffect(() => {
+    const q = query(collection(db, 'todos'))
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      let todosArr = []
+      querySnapshot.forEach((doc) => {
+        todosArr.push({...doc.data(), id: doc.id})
+      })
+      setTodos(todosArr)
+    })
+    return () => unsubscribe()
+  }, []);
+
+  // Update todo in firebase
+  const toggleComplete = async (todo) => {
+    await updateDoc(doc(db, 'todos', todo.id), {
+      completed: !todo.completed
+    })
+  }
+
+  // Delete todo
 
   return (
     <div className='h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]'>
       <div className='bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4'>
         <h3 className='text-3xl font-bold text-center text-gray-800 p-2'>Todo App</h3>
-        <form className='flex justify-between'>
-          <input className='border p-2 w-full text-xl' type="text" placeholder='Add Todo'/>
+        <form onSubmit={createTodo} className='flex justify-between'>
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            className='border p-2 w-full text-xl'
+            type="text"
+            placeholder='Add Todo'/>
           <button className='border p-4 ml-2 bg-purple-500 text-slate-100'>
             <AiOutlinePlus size={30}/>
           </button>
         </form>
         <ul>
           {todos.map((todo, index) => (
-            <Todo key={index} todo={todo}/>
+            <Todo key={index} todo={todo} toggleComplete={toggleComplete}/>
           ))}
         </ul>
         <p className='text-center p-2'>You have 2 todos</p>
